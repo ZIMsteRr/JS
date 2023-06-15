f693ee84e46d1a3993d687905d2547f06616f0a4
ZIMsteRr <126894797+ZIMsteRr@users.noreply.github.com>
2023-06-15 14:40:38 +0300

    new

---
+++ lesson_2/index2.js
@@ -0,0 +1,31 @@
+const operation = prompt("Choose your operation (+, -, *, /)");
+
+let firstOperand = prompt("Enter first operand");
+let secondOperand = prompt("Enter second operand");
+
+let result;
+let operationString;
+switch (operation) {
+  case "+":
+    result = firstOperand + secondOperand;
+    operationString = 'addition';
+    break;
+  case "-":
+    result = firstOperand - secondOperand;
+    operationString = 'subtraction';
+    break;
+  case "*":
+    result = firstOperand * secondOperand;
+    operationString = 'multiplication';
+    break;
+  case "/":
+    result = firstOperand / secondOperand;
+    operationString = 'division';
+    break;
+  default:
+    alert("Wrong operation!");
+}
+
+let message = operationString + ': ' + firstOperand + " " + operation + " " + secondOperand + " = " + result;
+
+alert(message);
